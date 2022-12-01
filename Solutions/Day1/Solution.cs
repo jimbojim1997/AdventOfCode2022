821385ba82d3be1c22f7bac7e0c2d61a503e537d
James Duda <jimbojim1997@gmail.com>
2022-12-01 22:24:39 +0000

    Refactored solution for Day1 Part1.

--- Solutions/Day1/Solution.cs
+++ Solutions/Day1/Solution.cs
@@ -1,36 +1,17 @@
-﻿namespace AdventOfCode2022.Solutions.Day1
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2022.Solutions.Day1
 {
     public static class Solution
     {
         public static int SolvePart1(string input)
         {
-            int maxValue = int.MinValue;
-            int maxIndex = 0;
-
-            int currentValue = 0;
-            int currentIndex = 1;
-
-            string[] lines = input.Split('\n');
-            foreach (string line in lines)
-            {
-                if (line == "")
-                {
-                    if (currentValue > maxValue)
-                    {
-                        maxValue = currentValue;
-                        maxIndex = currentIndex;
-                    }
-
-                    currentValue = 0;
-                    currentIndex++;
-                }
-                else if (int.TryParse(line, out int value))
-                {
-                    currentValue += value;
-                }
-            }
-
-            return maxValue;
+            return input.Split("\n\n")
+                        .Select(e => e.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(c => int.Parse(c))
+                                      .Sum())
+                        .Max();
         }
     }
 }
