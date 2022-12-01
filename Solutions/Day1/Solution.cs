fab70b471ef2c09198c342dab6ae5de146b2a73e
James Duda <jimbojim1997@gmail.com>
2022-12-01 22:12:11 +0000

    Solved Day1.

--- Solutions/Day1/Solution.cs
+++ Solutions/Day1/Solution.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Solutions.Day1
+﻿namespace Solutions.Day1
 {
     public static class Solution
     {
+        public static int Solve(string input)
+        {
+            int maxValue = int.MinValue;
+            int maxIndex = 0;
+
+            int currentValue = 0;
+            int currentIndex = 1;
+
+            string[] lines = input.Split('\n');
+            foreach (string line in lines)
+            {
+                if (line == "")
+                {
+                    if (currentValue > maxValue)
+                    {
+                        maxValue = currentValue;
+                        maxIndex = currentIndex;
+                    }
+
+                    currentValue = 0;
+                    currentIndex++;
+                }
+                else if (int.TryParse(line, out int value))
+                {
+                    currentValue += value;
+                }
+            }
+
+            return maxValue;
+        }
     }
 }
