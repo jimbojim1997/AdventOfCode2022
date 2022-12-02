dd18f5e134220972428b4b66018fed03e04c7838
James Duda <jimbojim1997@gmail.com>
2022-12-02 22:32:30 +0000

    Solved Day2 Part2.

--- Solutions/Day2.cs
+++ Solutions/Day2.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel.Design;
 
 namespace AdventOfCode2022.Solutions
 {
@@ -9,7 +8,7 @@ namespace AdventOfCode2022.Solutions
         public static int SolvePart1(string input)
         {
             int totalScore = 0;
-            foreach(string line in input.Split("\n"))
+            foreach (string line in input.Split("\n"))
             {
                 string[] hands = line.Split(' ');
                 string a = hands[0];
@@ -23,7 +22,62 @@ namespace AdventOfCode2022.Solutions
                     "X" => 1,
                     "Y" => 2,
                     "Z" => 3,
-                    _ => throw new InvalidOperationException($"The hand {hands[1]} is not valid.")
+                    _ => throw new InvalidOperationException($"The hand {b} is not valid.")
+                };
+            }
+
+            return totalScore;
+        }
+
+        public static int SolvePart2(string input)
+        {
+            int totalScore = 0;
+            foreach (string line in input.Split("\n"))
+            {
+                string[] hands = line.Split(' ');
+                string a = hands[0];
+                string b = hands[1];
+                string c;
+
+                if (b == "X")
+                {
+                    c = a switch
+                    {
+                        "A" => "Z",
+                        "B" => "X",
+                        "C" => "Y",
+                        _ => throw new InvalidOperationException($"The hand {a} is not valid.")
+                    };
+                }
+                else if (b == "Y")
+                {
+                    totalScore += 3;
+                    c = a switch
+                    {
+                        "A" => "X",
+                        "B" => "Y",
+                        "C" => "Z",
+                        _ => throw new InvalidOperationException($"The hand {a} is not valid.")
+                    };
+                }
+                else
+                {
+                    totalScore += 6;
+                    c = a switch
+                    {
+                        "A" => "Y",
+                        "B" => "Z",
+                        "C" => "X",
+                        _ => throw new InvalidOperationException($"The hand {a} is not valid.")
+                    };
+                }
+
+                totalScore += c switch
+                {
+                    "X" => 1,
+                    "Y" => 2,
+                    "Z" => 3,
+                    _ => throw new InvalidOperationException($"The hand {c} is not valid.")
                 };
             }
 
