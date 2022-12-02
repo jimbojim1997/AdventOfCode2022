08902c54d9845a6299813751cbc05557ff43d511
James Duda <jimbojim1997@gmail.com>
2022-12-02 22:21:00 +0000

    Solved Day1 Part1.

---
+++ Solutions/Day2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace AdventOfCode2022.Solutions
+{
+    //https://adventofcode.com/2022/day/2
+    public static class Day2
+    {
+        public static int SolvePart1(string input)
+        {
+            int totalScore = 0;
+            foreach(string line in input.Split("\n"))
+            {
+                string[] hands = line.Split(' ');
+                string a = hands[0];
+                string b = hands[1];
+
+                if ((a == "A" && b == "X") || (a == "B" && b == "Y") || (a == "C" && b == "Z")) totalScore += 3;
+                else if ((a == "A" && b == "Y") || (a == "B" && b == "Z") || (a == "C" && b == "X")) totalScore += 6;
+
+                totalScore += b switch
+                {
+                    "X" => 1,
+                    "Y" => 2,
+                    "Z" => 3,
+                    _ => throw new InvalidOperationException($"The hand {hands[1]} is not valid.")
+                };
+            }
+
+            return totalScore;
+        }
+    }
+}
