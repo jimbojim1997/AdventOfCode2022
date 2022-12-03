a952c68fae05728a54aabbcf332f04414bacfe82
James Duda <jimbojim1997@gmail.com>
2022-12-03 15:25:44 +0000

    Solved Day3 Part2.

--- Solutions/Day3.cs
+++ Solutions/Day3.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace AdventOfCode2022.Solutions
 {
@@ -18,6 +19,35 @@ namespace AdventOfCode2022.Solutions
                 foreach (char item in first)
                 {
                     if (second.Contains(item))
+                    {
+                        total += item switch
+                        {
+                            >= 'a' and <= 'z' => item - 'a' + 1,
+                            >= 'A' and <= 'Z' => item - 'A' + 27,
+                            _ => throw new InvalidOperationException($"The item {item} is not valid.")
+                        };
+                        break;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+        public static int SolvePart2(string input)
+        {
+            int total = 0;
+
+            string[] packs = input.Split('\n');
+            for (int i = 0; i < packs.Length; i += 3)
+            {
+                string a = packs[i];
+                string b = packs[i + 1];
+                string c = packs[i + 2];
+
+                foreach(char item in a)
+                {
+                    if (b.Contains(item) && c.Contains(item))
                     {
                         total += item switch
                         {
