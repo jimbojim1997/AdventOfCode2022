8dfc0903d3c9c7d18968df286bcc1c9d682f3172
James Duda <jimbojim1997@gmail.com>
2022-12-06 20:04:06 +0000

    Solved Day6 Part1.

---
+++ Solutions/Day6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AdventOfCode2022.Solutions
+{
+    //https://adventofcode.com/2022/day/6
+    public static class Day6
+    {
+        public static int SolvePart1(string input)
+        {
+            int duplicates = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                for(int b = 1; b <= 3 && i - b >= 0; b++)
+                {
+                    if (input[i] == input[i - b])
+                    {
+                        duplicates |= 1 << b;
+                        break;
+                    }
+                }
+
+                if (i > 3 && (duplicates & 0b1111) == 0) return i + 1;
+
+                duplicates <<= 1;
+            }
+
+            throw new InvalidOperationException("Input doesn't contain a valid start-of-packet marker.");
+        }
+    }
+}
