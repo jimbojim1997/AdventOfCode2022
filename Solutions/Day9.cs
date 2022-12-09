ef883588d8586496dac4fab9f1645ac29cbd182d
James Duda <jimbojim1997@gmail.com>
2022-12-09 23:14:16 +0000

    Solved Day9 Part1.

---
+++ Solutions/Day9.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022.Solutions
+{
+    public static class Day9
+    {
+        public static int SolvePart1(string input)
+        {
+            (int x, int y) head = (0, 0);
+            (int x, int y) tail = (0, 0);
+
+            var touchedPositions = new HashSet<(int, int)> { tail };
+            foreach (var instruction in input.Split('\n'))
+            {
+                string[] parts = instruction.Split(' ');
+                string direction = parts[0];
+                int.TryParse(parts[1], out int count);
+
+                for (int i = 0; i < count; i++)
+                {
+                    switch (direction)
+                    {
+                        case "U":
+                            head = (head.x, head.y + 1);
+                            break;
+                        case "D":
+                            head = (head.x, head.y - 1);
+                            break;
+                        case "L":
+                            head = (head.x - 1, head.y);
+                            break;
+                        case "R":
+                            head = (head.x + 1, head.y);
+                            break;
+                    }
+
+                    if (Diff(head.x, tail.x) > 1 || Diff(head.y, tail.y) > 1)
+                    {
+                        tail = (tail.x + Math.Sign(head.x - tail.x), tail.y + Math.Sign(head.y - tail.y));
+                        touchedPositions.Add(tail);
+                    }
+                }
+            }
+
+            return touchedPositions.Count;
+        }
+
+        public static int Diff(int a, int b)
+        {
+            if (a > b) return a - b;
+            else return b - a;
+        }
+    }
+}
---
+++ Tests/Day9.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode2022.Tests
+{
+    internal class Day9
+    {
+        [Theory]
+        [TestCase("Day9Part1Example.txt", 13)]
+        [TestCase("Day9Part1Puzzle.txt", 5902)]
+        public void SolvePart1(string inputFile, int expected)
+        {
+            string input = File.ReadAllText(inputFile);
+            int actual = Solutions.Day9.SolvePart1(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
