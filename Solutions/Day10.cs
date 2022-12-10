4d5c379e6da30206190b98e826c69d457f50dd5b
James Duda <jimbojim1997@gmail.com>
2022-12-10 20:20:35 +0000

    Solved Day10 Part1.

---
+++ Solutions/Day10.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace AdventOfCode2022.Solutions
+{
+    public static class Day10
+    {
+        public static int SolvePart1(string input)
+        {
+            var lines = input.Split('\n');
+            var enumerator = lines.GetEnumerator();
+
+            int result = 0;
+            string[] instruction = Array.Empty<string>();
+            int cyclesRaminaing = 0;
+            int regX = 1;
+            for (int cycle = 1; ; cycle++)
+            {
+                if ((cycle + 20) % 40 == 0)
+                {
+                    result += cycle * regX;
+                }
+
+                if (cyclesRaminaing == 0)
+                {
+                    if (!enumerator.MoveNext()) break;
+                    instruction = (enumerator.Current as string)!.Split(' ');
+
+                    cyclesRaminaing = instruction[0] switch
+                    {
+                        "noop" => 1,
+                        "addx" => 2,
+                        _ => throw new InvalidOperationException($"The instruction {instruction[0]} is not valid.")
+                    };
+                }
+
+                if (cyclesRaminaing == 1)
+                {
+                    switch (instruction[0])
+                    {
+                        case "noop":
+                            break;
+                        case "addx":
+                            int value = int.Parse(instruction[1]);
+                            regX += value;
+                            break;
+
+                    }
+                }
+
+                cyclesRaminaing--;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/Day10.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode2022.Tests
+{
+    internal class Day10
+    {
+        [Theory]
+        [TestCase("Day10Part1Example.txt", 13140)]
+        [TestCase("Day10Part1Puzzle.txt", 15360)]
+        public void SolvePart1(string inputFile, int expected)
+        {
+            string input = File.ReadAllText(inputFile);
+            int actual = Solutions.Day10.SolvePart1(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
