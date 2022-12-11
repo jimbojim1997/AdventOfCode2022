9ceb339175c5556a5b32b4302bda9f9da28033bf
James Duda <jimbojim1997@gmail.com>
2022-12-11 17:37:58 +0000

    Solved Day11 Part2.

--- Solutions/Day11.cs
+++ Solutions/Day11.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode2022.Solutions
         public static int SolvePart1(string input)
         {
             string[] inputSections = input.Split("\n\n");
-            var monkeys = inputSections.Select(s => new Monkey(s)).ToArray();
+            var monkeys = inputSections.Select(s => new Monkey1(s)).ToArray();
 
             for (int round = 0; round < 20; round++)
             {
@@ -27,20 +27,44 @@ namespace AdventOfCode2022.Solutions
                 }
             }
 
-            return monkeys.OrderByDescending(m => m.NumberOfInspecitons).Take(2).Aggregate(1, (a, m) => a * m.NumberOfInspecitons);
+            return monkeys.OrderByDescending(m => m.NumberOfInspecitons).Take(2).Aggregate(1, (a, m) => a * (int)m.NumberOfInspecitons);
         }
 
-        private class Monkey
+        public static long SolvePart2(string input)
+        {
+            string[] inputSections = input.Split("\n\n");
+            var monkeys = inputSections.Select(s => new Monkey2(s)).ToArray();
+
+            int shrink = monkeys.Select(m => m.TestDivisor).Aggregate(1, (a, n) => a * n);
+
+            for (int round = 0; round < 10_000; round++)
+            {
+                foreach (var monkey in monkeys)
+                {
+                    while (monkey.Items.TryDequeue(out long currentItem))
+                    {
+                        monkey.NumberOfInspecitons++;
+                        long worryLevel = monkey.WorryLevelChange(currentItem) % shrink;
+                        int nextMonkey = monkey.GetNextMonkey(worryLevel);
+                        monkeys[nextMonkey].Items.Enqueue(worryLevel);
+                    }
+                }
+            }
+
+            return monkeys.OrderByDescending(m => m.NumberOfInspecitons).Take(2).Aggregate((long)1, (a, m) => a * m.NumberOfInspecitons);
+        }
+
+        private class Monkey1
         {
             private static readonly Regex _regexParse = new Regex(@"(?'monkey'Monkey (?'id'\d+):\s+Starting items: (?:(?'starting_item'\d+)(?:, )?)+)\s+Operation: new = (?'op1'.*?) (?'op2'.*?) (?'op3'.*?)\s+Test: divisible by (?'test'\d+)\s+If true: throw to monkey (?'throw_true'\d+)\s+If false: throw to monkey (?'throw_false'\d+)", RegexOptions.Compiled);
 
             public readonly int Id;
-            public int NumberOfInspecitons = 0;
+            public long NumberOfInspecitons = 0;
             public readonly Queue<int> Items = new Queue<int>();
             public readonly Func<int, int> WorryLevelChange; //Operation in puzzle
             public readonly Func<int, int> GetNextMonkey; //Test in puzzle
 
-            public Monkey(string input)
+            public Monkey1(string input)
             {
                 var match = _regexParse.Match(input);
                 Id = int.Parse(match.Groups["id"].ValueSpan);
@@ -80,6 +104,61 @@ namespace AdventOfCode2022.Solutions
                     int ifTrue = int.Parse(match.Groups["throw_true"].ValueSpan);
                     int ifFalse = int.Parse(match.Groups["throw_false"].ValueSpan);
                     GetNextMonkey = (old) => old % divisiableBy == 0 ? ifTrue : ifFalse;
+                }
+            }
+        }
+
+        private class Monkey2
+        {
+            private static readonly Regex _regexParse = new Regex(@"(?'monkey'Monkey (?'id'\d+):\s+Starting items: (?:(?'starting_item'\d+)(?:, )?)+)\s+Operation: new = (?'op1'.*?) (?'op2'.*?) (?'op3'.*?)\s+Test: divisible by (?'test'\d+)\s+If true: throw to monkey (?'throw_true'\d+)\s+If false: throw to monkey (?'throw_false'\d+)", RegexOptions.Compiled);
+
+            public readonly int Id;
+            public long NumberOfInspecitons = 0;
+            public readonly Queue<long> Items = new Queue<long>();
+            public readonly Func<long, long> WorryLevelChange; //Operation in puzzle
+            public readonly Func<long, int> GetNextMonkey; //Test in puzzle
+            public readonly int TestDivisor;
+
+            public Monkey2(string input)
+            {
+                var match = _regexParse.Match(input);
+                Id = int.Parse(match.Groups["id"].ValueSpan);
+
+                foreach (Capture cap in match.Groups["starting_item"].Captures) Items.Enqueue(int.Parse(cap.ValueSpan));
+
+                {
+                    Func<long, long> left = (old) => old;
+
+                    string opName = match.Groups["op2"].Value;
+                    Func<long, long, long> op = opName switch
+                    {
+                        "+" => (l, r) => l + r,
+                        "-" => (l, r) => l - r,
+                        "*" => (l, r) => l * r,
+                        "/" => (l, r) => l / r,
+                        _ => throw new InvalidOperationException($"The operation {opName} is not valid.")
+                    };
+
+                    string rightValue = match.Groups["op3"].Value;
+                    Func<long, long> right;
+                    if (rightValue == "old")
+                    {
+                        right = (old) => old;
+                    }
+                    else
+                    {
+                        int rightNumeric = int.Parse(rightValue);
+                        right = (old) => rightNumeric;
+                    }
+
+                    WorryLevelChange = (old) => op(left(old), right(old));
+                }
+
+                {
+                    TestDivisor = int.Parse(match.Groups["test"].ValueSpan);
+                    int ifTrue = int.Parse(match.Groups["throw_true"].ValueSpan);
+                    int ifFalse = int.Parse(match.Groups["throw_false"].ValueSpan);
+                    GetNextMonkey = (old) => old % TestDivisor == 0 ? ifTrue : ifFalse;
                 }
             }
         }
--- Tests/Day11.cs
+++ Tests/Day11.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2022.Tests
+﻿namespace AdventOfCode2022.Tests
 {
     internal class Day11
     {
@@ -15,6 +9,17 @@ namespace AdventOfCode2022.Tests
         {
             string input = File.ReadAllText(inputFile);
             int actual = Solutions.Day11.SolvePart1(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Theory]
+        [TestCase("Day11Example.txt", 2713310158)]
+        [TestCase("Day11Puzzle.txt", 25590400731)]
+        public void SolvePart2(string inputFile, long expected)
+        {
+            string input = File.ReadAllText(inputFile);
+            long actual = Solutions.Day11.SolvePart2(input);
 
             Assert.That(actual, Is.EqualTo(expected));
         }
